e15f46cb639445dc8a99aeb809d7c50de1ff7abc
digitalneedstech <69498018+digitalneedstech@users.noreply.github.com>
2024-08-22 12:11:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 import json 
-import os
+import os 
 from typing import List
 
 import click
